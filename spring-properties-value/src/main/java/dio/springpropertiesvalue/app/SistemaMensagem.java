925f36e3a6bd6ca840463919fedfadc94db7ae15
gabrielrsh <gabriel.shiota@gmail.com>
2023-02-02 12:35:58 -0300

    adiciona exemplo de properties value

---
+++ spring-properties-value/src/main/java/dio/springpropertiesvalue/app/SistemaMensagem.java
@@ -0,0 +1,27 @@
+package dio.springpropertiesvalue.app;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SistemaMensagem implements CommandLineRunner {
+    @Value("${nome:ValorPadrao}")
+    private String nome;
+    @Value("${email}")
+    private String email;
+    @Value("${telefones}")
+    private List<Long> telefones;
+    
+    
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Mensagem enviada por: "+ nome +
+            "\nE-mail: " + email +
+            "\nCom telefones para contato: "+telefones);
+        System.out.println("Seu cadastro foi aprovado");
+    }
+
+
+}
